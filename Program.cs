2b4f410e4e8035c4efc3090381aa91bb22dcfb43
kdenespg <kovacs.denes@szbi-pg.hu>
2025-09-29 13:39:45 +0200

    x

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace KD_Gokart
+{
+    internal class Program
+        {
+            static void Main(string[] args)
+        {
+            string filePath = "vezeteknevek.txt"; // A fájl elérési útja
+
+            try
+            {
+                // Teljes fájl beolvasása
+                string content = File.ReadAllText(filePath);
+                Console.WriteLine("A fájl tartalma:");
+                Console.WriteLine(content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba történt a fájl beolvasása során:");
+                Console.WriteLine(ex.Message);
+            }
+
+            
+        }
+    }
+}
